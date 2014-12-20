07571c0c6aa78b6f48b84efbdb9e3001bb4c1bd1
timoho98 <timoho98@gmail.com>
2014-12-19 23:45:24 -0800

    Added Video Processing Command Args only runs if not loaded into module Added test templates for flask for web gui Trying some things out for flask

--- InstabotWebGui.py
+++ InstabotWebGui.py
@@ -1,11 +1,13 @@
 __author__ = 'Timothy'
+import Instagrambot
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("Index.html")
 @app.route('/testrender', methods = ['POST' , 'GET'])
 def testrender():
     if request.method == 'POST':
@@ -16,4 +18,13 @@ def testrender():
         else:
             print "placeholder"
     return render_template("test.html")
+@app.route('/listusers')
+def listusers():
+    return render_template("idlist.html", idlist = Instagrambot.jsoniddata)
+@app.route('/checkuser', methods = ['POST', 'GET'])
+def checkuser():
+    if request.method == 'POST':
+        print request.form['submit']
+    return render_template("usercheck.html", idlist = Instagrambot.jsoniddata, lastdate = Instagrambot.getlistiddate())
 app.run()
+
--- Instagrambot.py
+++ Instagrambot.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 import json, requests, datetime, praw, ConfigParser, sys, getopt, os, copy
 #jexec -u root -n PythonScript sh python /mnt/script/InstagramRedditBot/Instagrambot.py -h
+current_path = os.path.abspath(os.path.dirname(__file__))
 # Get your key/secret from http://instagram.com/developer/
 config = ConfigParser.ConfigParser()
-config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.ini'))
+config.read(os.path.join(current_path, 'config.ini'))
 INSTAGRAM_CLIENT_ID = config.get("instagram", "clientid")
 INSTAGRAM_CLIENT_SECRET = config.get("instagram", "clientsecret")
 IMGUR_CLIENT_ID = config.get("imgur", "clientid")
@@ -20,15 +21,15 @@ currentunixtimestamp = datetime.datetime.utcnow()
 #TODO Video processing
 #TODO Error Handle for instagram and Reddit
 
-#file loading
-#loading Json from target file
-#print os.path.join(os.path.abspath(os.path.dirname(__file__)), TARGET_JSON_FILE)
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), TARGET_JSON_FILE)) as Jsonidfile:
-    jsoniddata = json.load(Jsonidfile)
-updatedjson = copy.deepcopy(jsoniddata)
 r = praw.Reddit(user_agent=REDDIT_USERAGENT)
 r.login(REDDIT_USERNAME, REDDIT_PASSWORD)
 postsubreddit = praw.objects.Subreddit(reddit_session= r, subreddit_name= REDDIT_SUBREDDIT)
+#file loading
+#loading Json from target file
+#print os.path.join(os.path.abspath(os.path.dirname(__file__)), TARGET_JSON_FILE)
+with open(os.path.join(current_path, TARGET_JSON_FILE)) as Jsonidfile:
+    jsoniddata = json.load(Jsonidfile)
+updatedjson = copy.deepcopy(jsoniddata)
 #submissionobj = postsubreddit.get_flair_choices(link = 'http://redd.it/2ocgcv')
 #print submissionobj
 
@@ -38,7 +39,7 @@ def logstuff (text): #automaticly adds the new like '\n' at the end of every ent
     currentdaymonthyear = str(currentunixtimestamp.year) + "-" + str(currentunixtimestamp.month) + "-" + str(currentunixtimestamp.day)
     #print currentdaymonthyear
     print ("Logged: " + text + " in " + currentdaymonthyear + ".log at " + currenttime)
-    log = open(os.path.join(os.path.abspath(os.path.dirname(__file__)), currentdaymonthyear + ".log"), "ab")
+    log = open(os.path.join(current_path, currentdaymonthyear + ".log"), "ab")
     log.write(currentdaymonthyear + " " + currenttime + ": " +text + '\n')
     log.close()
 
@@ -96,21 +97,40 @@ def getendoflink(link):
 
 #List Ids that exist in the json file
 def getlistid():
+    idstring = ''
     for id in jsoniddata:
-        print id['name']
+        idstring += id['name']
+        idstring += '\n'
+    return idstring
+
+#list ids as well as last dates
+def getlistiddate():
+    iddatestring = []
+    for id in jsoniddata:
+        iddatestring.append(id['name'] + ' - ' + str(datetime.datetime.fromtimestamp(int(id['lastdate']))) + '\n')
+    return iddatestring
 
 #Get json of most recent media param Userid dict from json
 def getmediaJSON (userid):
     mediareturn = requests.get('https://api.instagram.com/v1/users/' + str(userid['userid']) + '/media/recent/?client_id=' + INSTAGRAM_CLIENT_ID)
-    logstuff("Starting request at of InstaId:" + str(userid['name']) + " User Id:" +str(userid['userid']))
+    logstuff("Request JSON of InstaId:" + str(userid['name']) + " User Id:" +str(userid['userid']))
     #print "Requested from " + str(userid['userid'])
     mediareturnJSON = mediareturn.json()
     return mediareturnJSON
 
 #Generates Text for comment
 def generatecommenttext(source):
-    commentstring = "[Source](" + source  + ")"
+    commentstring = "[Source](" + source  + ")" #format source text
     return commentstring
+'''
+Expected output for comment
+Source #as link
+Created Time (Unix Epoch Time
+Filter:
+Tags
+if video thumbnail
+allow for true false if should be enabled
+'''
 
 #post to subreddit, param url, and add comment at the end of submission
 def submittoreddit(url,  linkcaption, source):
@@ -125,12 +145,13 @@ def checkimage (media , idjson):
     Instagram_username = media['user']['username']
     lastdate = idjson['lastdate']
     if int(media['created_time']) > int(lastdate): #check date then check filetype
-        logstuff("New Image for " + Instagram_username +" found at :" + media['link'] + " created date:" + datetime.datetime.fromtimestamp(int(media['created_time'])).strftime('%Y-%m-%d %H:%M:%S') + " after Last-Date:" + datetime.datetime.fromtimestamp(int(lastdate)).strftime('%Y-%m-%d %H:%M:%S'))
+        logstuff("New Media for " + Instagram_username +" found at :" + media['link'] + " created date:" + datetime.datetime.fromtimestamp(int(media['created_time'])).strftime('%Y-%m-%d %H:%M:%S') + " after Last-Date:" + datetime.datetime.fromtimestamp(int(lastdate)).strftime('%Y-%m-%d %H:%M:%S'))
         return True
     else:
         #print media['link'] +" " +media['created_time'] + ' olderthan' + lastdate
         return False
 
+#OUTDATED USELESS
 #check if json is video or image, returns a 1 if image, returns a 2 if video
 def checktype (imagemedia):
     if imagemedia['type'] == 'image':
@@ -139,29 +160,39 @@ def checktype (imagemedia):
         return 2
 
 #process the media given a media json
-def processimage (imagemedia):
-    Instagram_username = imagemedia['user']['username']
+def processimage (imagejson):
+    Instagram_username = imagejson['user']['username']
     #gather data from media obj
-    imageurl = imagemedia['images']['standard_resolution']['url']
-    if imagemedia['caption'] is None:  #check if there is a caption or not
+    imageurl = imagejson['images']['standard_resolution']['url']
+    if imagejson['caption'] is None:  #check if there is a caption or not
         print "Text/title dne"
         caption = "No Caption"
     else:
-        caption = imagemedia['caption']['text']
+        caption = imagejson['caption']['text']
         #print 'Caption:' + caption #Removed because cant print special chars
     #print str(imagemedia['caption'])
-    logstuff("Starting Upload of image " + str(imagemedia['link']))
+    logstuff("Starting Upload of image " + str(imagejson['link']))
     imgurjson = imgurupload(imageurl, caption)
     if imgurjson['success']:
-        submittedlink = submittoreddit(url = imgurjson['data']['link'], linkcaption = caption, source= imagemedia['link'])
+        submittedlink = submittoreddit(url = imgurjson['data']['link'], linkcaption = caption, source= imagejson['link'])
         r.select_flair(item = submittedlink, flair_template_id= chooseflair(username = Instagram_username))
         logstuff("Selected flair for " + Instagram_username)
         #flair_template_id= 'd1e51b54-5fb1-11e4-a579-12313b0e5086') #flair_template_id= chooseflair(username = Instagram_username))
+    logstuff("Finished Processing image for " + Instagram_username + " Imageid: " + imagejson['id'])
 
 #do processing for video
-def processvideo(imagemedia):
-    videourl = imagemedia['link']
-    print "placeholder"
+def processvideo(videojson):
+    Instagram_username = videojson['user']['username']
+    videourl = videojson['videos']['standard_resolution']['url']
+    if videojson['caption'] is None: #check if there is a caption or not
+        print "Text/title dne"
+        caption = "No Caption"
+    else:
+        caption = videojson['caption']['text']
+    submittedlink = submittoreddit(url = videourl, linkcaption= caption, source= videojson['link'])
+    r.select_flair(item = submittedlink, flair_template_id= chooseflair(username = Instagram_username))
+    logstuff("Selected flair for " + Instagram_username)
+    logstuff("Finished Processing video for " + Instagram_username + " Videoid:" + videojson['id'])
 
 #check instagram for new pictures with particular id
 def updatewithid (iddict):
@@ -172,45 +203,48 @@ def updatewithid (iddict):
     #print "Print mediajson" + str(mediaJSON)
     for m in mediaJSON['data']:
         if checkimage(media = m, idjson = iddict):
-            if checktype(m) == 1:
+            if m['type'] == 'image':
                 processimage(imagemedia= m)
-            elif checktype(m) == 2:
+            elif m['type'] == 'video':
                 processvideo(m)
             lastpartoflink = getendoflink(m['link'])
             writetodatejson(date = m['created_time'], username= m['user']['username'])
 
-arguments = sys.argv[1:] #get arguments after the command run
-try:
-    opts, args = getopt.getopt(arguments, 'tu:ch', ['test', 'user=', 'check', 'help'])
-except getopt.GetoptError:
-    print ('arg not recongnized')
-for opt, arg in opts:
-    if opt in ('-t, --test'):
-        print 'test'
-    elif opt in ('-u', '--user'):
-        if checkifidindata(username=arg):
-            for id in jsoniddata: #parse through list to find the id there is probably a better way to do this or organize data better but idk what it is
-                if id['name'] == str(arg):
-                    updatewithid(id)
+if __name__ == "__main__": #only runs if not loaded as a module
+    arguments = sys.argv[1:] #get arguments after the command run
+    try:
+        opts, args = getopt.getopt(arguments, 'tu:chl', ['test', 'user=', 'check', 'help', 'list'])
+    except getopt.GetoptError:
+        print ('arg not recongnized')
+    for opt, arg in opts:
+        if opt in ('-t, --test'):
+            print 'test'
+        elif opt in ('-u', '--user'):
+            if checkifidindata(username=arg):
+                for id in jsoniddata: #parse through list to find the id there is probably a better way to do this or organize data better but idk what it is
+                    if id['name'] == str(arg):
+                        updatewithid(id)
+                with open(os.path.join(current_path, TARGET_JSON_FILE), 'wb') as newjsonfile: #part where we write to file
+                    json.dump(updatedjson, newjsonfile, indent = 4, separators=(', ', ': '))
+                logstuff("Updated Jsonfile")
+            else:
+                print "error " + arg + " does not exist in config"
+        elif opt in ('-c', '--check'):
+            logstuff("Running Check Command")
+            #logstuff("Running Check command at " + currentunixtimestamp.hour + ":" + currentunixtimestamp.minute + " on " + currentunixtimestamp.month + "-" + currentunixtimestamp.day)
+            for ids in jsoniddata:
+                updatewithid(ids)
+            with open(os.path.join(current_path, TARGET_JSON_FILE), 'wb') as newjsonfile: #part where we write to file
+                json.dump(updatedjson, newjsonfile, indent = 4, separators=(', ', ': '))
+            logstuff("Updated Jsonfile")
+        elif opt in ('-h', '--help'):
+            print 'The current commands are:'
+            print '--user, -u: Params: (Username), update a specific id'
+            print '--check, -c: No Params: basically go through all ids and check'
+        elif opt in ('-l', 'list'):
+            print getlistid()
         else:
-            print "error " + arg + " does not exist in config"
-    elif opt in ('-c', '--check'):
-        logstuff("Running Check Command")
-        #logstuff("Running Check command at " + currentunixtimestamp.hour + ":" + currentunixtimestamp.minute + " on " + currentunixtimestamp.month + "-" + currentunixtimestamp.day)
-        for ids in jsoniddata:
-            updatewithid(ids)
-    elif opt in ('-h', '--help', ''):
-        print 'The current commands are:'
-        print '--user, -u: Params: (Username), update a specific id'
-        print '--check, -c: No Params: basically go through all ids and check'
-    else:
-        print 'No command detected, please add -h or --help at the end of console command to bring up a list of commands'
-
-#write with updated json
-#Test Update String#updatedjson = writetodatejson(date = 230492, username= 'test', jsondict= jsoniddata)
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), TARGET_JSON_FILE), 'wb') as newjsonfile: #part where we write to file
-    json.dump(updatedjson, newjsonfile, indent = 4, separators=(', ', ': '))
-logstuff("Updated Jsonfile")
+            print 'No command detected, please add -h or --help at the end of console command to bring up a list of commands'
 
 
 
